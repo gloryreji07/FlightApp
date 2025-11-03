f5b0ec03548afe9ab3258934dc4f379bc9cba5d8
Glory Reji <glory.reji07@gmail.com>
2025-11-03 10:38:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3396840661-91fd7",
+  "appId": "1:143006548219:web:fedbb94ebb36a7b54d705e",
+  "apiKey": "AIzaSyAj7McUSu4nQVWv1MdMcUNJX4PT31mzokY",
+  "authDomain": "studio-3396840661-91fd7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "143006548219"
+};
